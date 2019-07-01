d5a07b703ce327089e4b325263b45ad1ba3b8580
Sergei Portsev <jiperscrip@gmail.com>
2019-07-01 15:52:31 +0400

    add router and reset.scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,24 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Route, Router, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
 import { store, persistor } from './store';
+import { MainPage } from './layouts/main-page/main-page';
+
+import './base-style/reset.scss';
+import './base-style/base.scss';
+
+const history: any = createBrowserHistory();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <div>Hello World!!!</div>
-    </PersistGate>
-  </Provider>,
+  <Router history={history}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Route path="/" component={MainPage} exact />
+      </PersistGate>
+    </Provider>
+  </Router>,
   document.getElementById('app')
 );
---
+++ src/layouts/main-page/main-page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import './main-page.scss';
+
+export class MainPage extends React.Component {
+  public render(): JSX.Element {
+    return <div>Hello World!!!</div>;
+  }
+}
